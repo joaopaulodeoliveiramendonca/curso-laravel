4f4374e546a5004286eeb963645d56e8776ab890
João Paulo <joaopaulodeoliveiramendonca@gmail.com>
2022-10-23 17:05:49 -0400

    up

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Meu titulo')</title>
-    <link rel="stylesheet" href="{{asset('/assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     @stack('styles')
 </head>
 <body>
     layout
     @yield('content')
 
-    <script src="{{asset('/assets/js/app.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
     @stack('scripts')
 </body>
 </html>
